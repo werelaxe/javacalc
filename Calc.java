4c25b63dfcb3be3e403bfb7c8c09c8a8588ca1f3
Werelaxe <integrex@yandex.ru>
2016-12-05 02:27:36 +0500

    start parse parentheses

--- Calc.java
+++ Calc.java
@@ -12,10 +12,39 @@ import java.util.Set;
  * Created by melon on 05.12.16.
  */
 public class Calc {
-    public static ArrayList<IProcessable> parse(String source) {
+    public static ArrayList<Token> removeWhitespaces(String source) {
         Lexer lexer = new Lexer();
         lexer.register("math", new MathGrammar());
         ArrayList<Token> lexems = lexer.getLexems("math", source);
+        ArrayList<Token> finalLexems = new ArrayList<>();
+        for (Token lexeme : lexems) {
+            if (!lexeme.getType().equals("whitespace"))
+                finalLexems.add(lexeme);
+        }
+        return finalLexems;
+    }
+    public static ArrayList<Token> preParse(ArrayList<Token> lexems) {
+        ArrayList<Token> finalLexems = new ArrayList<>();
+        int i = 0;
+        while (i < lexems.size()){
+            Token lexeme = lexems.get(i);
+            if (lexeme.getSubType().equals("left_parenthesis")) {
+                System.out.println("\nStart process pereths");
+                Token currentLexeme = new Token("none", "none");
+                while (i != lexems.size() && !"right_parenthesis".equals(currentLexeme.getSubType())) {
+                    currentLexeme = lexems.get(i);
+                    System.out.println(currentLexeme);
+                    i++;
+                }
+                System.out.println("Finish process pereths\n");
+            }
+            if (i != lexems.size())
+                System.out.println(lexems.get(i));
+            i++;
+        }
+        return null;
+    }
+    public static ArrayList<IProcessable> parse(ArrayList<Token> lexems) {
         ArrayList<IProcessable> finalSummands = new ArrayList<>();
         for (Token lexeme : lexems) {
             if (!lexeme.getType().equals("whitespace")) {
@@ -32,9 +61,7 @@ public class Calc {
         return finalSummands;
     }
     public static void calculate(String source) {
-        ArrayList<IProcessable> objects = parse(source);
-        for (IProcessable obj: objects) {
-            System.out.println(obj);
-        }
+        ArrayList<Token> objects = removeWhitespaces(source);
+        preParse(objects);
     }
 }
