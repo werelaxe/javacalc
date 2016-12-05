ff6f4f7c045a4f52d656fe7e4d4594578356a69f
Werelaxe <integrex@yandex.ru>
2016-12-05 14:28:37 +0500

    implemented adding

--- Operand.java
+++ Operand.java
@@ -2,21 +2,48 @@ package calculator;
 
 import vector.Vector;
 
+import java.time.temporal.ValueRange;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by melon on 05.12.16.
  */
 public class Operand implements IProcessable {
-    public Operand(int real, int imagine, Set<Vector> vectors) {
+    public Operand(int real, int imagine, HashSet<Vector> vectors) {
         this.real = real;
         this.imagine = imagine;
-        this.vectors = vectors;
+        this.vectors = new HashMap<Integer, Vector>();
+        for (Vector v : vectors) {
+            this.vectors.put(v.getDimCount(), v);
+        }
     }
 
     public int real;
     public int imagine;
-    public Set<Vector> vectors;
+    public HashMap<Integer, Vector> vectors;
+
+    public static Operand sum(Operand firstOperand, Operand secondOperand) {
+        int newReal = firstOperand.real + secondOperand.real;
+        int newImagine = firstOperand.imagine + secondOperand.imagine;
+        HashSet<Vector> vectors = new HashSet<>();
+        for (Vector vec : firstOperand.vectors.values()) {
+            if (!secondOperand.vectors.keySet().contains(vec.getDimCount())) {
+                vectors.add(vec);
+            } else {
+                Vector second = secondOperand.vectors.get(vec.getDimCount());
+                Vector finalVector = vec.add(second);
+                vectors.add(finalVector);
+            }
+        }
+        for (Vector vec : secondOperand.vectors.values()) {
+            if (!firstOperand.vectors.keySet().contains(vec.getDimCount())) {
+                vectors.add(vec);
+            }
+        }
+        return new Operand(newReal, newImagine, vectors);
+    }
 
     @Override
     public boolean isOperator() {
@@ -26,9 +53,11 @@ public class Operand implements IProcessable {
     @Override
     public String toString() {
         StringBuilder buffer = new StringBuilder();
-        for (Vector vector:vectors) {
+        for (Vector vector : vectors.values()) {
             buffer.append(vector);
+            buffer.append(" ");
         }
-        return String.format("Operand(%s, %s, %s)", real, imagine, buffer.toString());
+        String result = buffer.toString();
+        return String.format("Operand(%s, %s, %s)", real, imagine, result.substring(0, result.length() - 1));
     }
 }
--- Program.java
+++ Program.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import vector.Vector;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
 
 /**
@@ -16,6 +19,23 @@ public class Program {
             input = "2 + 2 * 2";
         }
         //System.out.println("Calculating " + input);
-        Calc.calculate(input);
+        //Calc.calculate(input);
+        HashSet<Vector> vectors1 = new HashSet<>();
+        vectors1.add(new Vector(new int[]{3, 1}));
+        vectors1.add(new Vector(new int[]{3, 1, 0}));
+
+        HashSet<Vector> vectors2 = new HashSet<>();
+        vectors2.add(new Vector(new int[]{4, 5, 6}));
+        vectors2.add(new Vector(new int[]{4, 5, 6, 3}));
+        vectors2.add(new Vector(new int[]{4}));
+
+        HashSet<Vector> vectors3 = new HashSet<>();
+        vectors3.add(new Vector(new int[]{0, 5, 1, 3, 4}));
+        vectors3.add(new Vector(new int[]{0, 5}));
+
+        Operand op1 = new Operand(-1, 3, vectors1);
+        Operand op2 = new Operand(0, 4, vectors2);
+        Operand op3 = new Operand(10, -2, vectors3);
+        System.out.println(Operand.sum(Operand.sum(op1, op2), op3));
     }
 }
